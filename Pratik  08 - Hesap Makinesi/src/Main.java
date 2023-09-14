63e30b94a1e1e4b8ba5cc34885b38543242d3207
Mehmet Batuhan Polat <batuhanpolat2@hotmail.com>
2023-09-14 17:18:53 +0300

    Code Added

--- Pratik  08 - Hesap Makinesi/src/Main.java
+++ Pratik  08 - Hesap Makinesi/src/Main.java
@@ -1,10 +1,43 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello ansdfadsfsdfome!");
+        Scanner input = new Scanner(System.in);
 
+        //Değişkenler Oluşturuldu
+        double numberOne, numberTwo, result;
+        int selection;
+
+        //Kullanıcıdan Veri İstendi
+        System.out.print("1. Sayıyı Giriniz : ");
+        numberOne = input.nextDouble();
+
+        System.out.print("2. Sayıyı Giriniz : ");
+        numberTwo = input.nextDouble();
+
+        System.out.print("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme\nYapılmak istenen işlem : ");
+        selection = input.nextInt();
+
+        //Switch Yapısı Kullanılarak Hesaplama Sağlandı
+        switch (selection) {
+            case 1:
+                result = numberOne + numberTwo;
+                System.out.println("Sonuç : " + result);
+                break;
+            case 2:
+                result = numberOne - numberTwo;
+                System.out.println("Sonuç : " + result);
+                break;
+            case 3:
+                result = numberOne * numberTwo;
+                System.out.println("Sonuç : " + result);
+                break;
+            case 4:
+                result = numberOne / numberTwo;
+                System.out.println("Sonuç : " + result);
+                break;
+            default:
+                System.out.println("Hatalı Tuşlama Yaptınız!");
+        }
     }
 }
